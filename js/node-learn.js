a35ccae7356da8a541926bf02de8f24842261d23
Kirill <yakubov.kirill.work@gmail.com>
2020-09-15 23:08:15 +0300

    feat: http and pages

--- js/node-learn.js
+++ js/node-learn.js
@@ -3,15 +3,57 @@ const path = require('path');
 const os = require('os');
 const events = require('events');
 const { EventEmitter } = require('events');
+const http = require('http');
+const fs = require('fs');
 
 //console.log(path.parse(__filename));
 //console.log(os.platform());
 //console.log(os.cpus());
 //console.log(os.networkInterfaces());
-
+/*
 const emiter = new EventEmitter();
 emiter.on('any', data => {
   console.log('On: ', data)
 });
 
-emiter.emit('any', {a: 1});
+emiter.emit('any', {a: 1});*/
+
+const server = http.createServer((req, res) => {
+  
+  if (req.url === '/') {
+    fs.readFile(path.join(__dirname, '../index.html'), (err, data) => {
+      if(err) throw err;
+
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+
+      res.end(data);
+    });
+  };
+
+  let filePath = path.join(__dirname, req.url === '/' ? 'index.html': req.url);
+  fs.readFile(filePath, (err, data) => {
+    if(err) {
+        fs.readFile(path.join(__dirname, '../err.html'), (err, errData) => {
+          if (err) throw err;
+
+          res.writeHead(200, {
+            'Content-Type': 'text/html'
+          });
+
+          res.end(errData);
+      });
+    } else {
+      res.writeHead(200, {
+        'Content-Type': 'text/html'
+      });
+
+      res.end(data);
+    }
+  });
+});
+
+server.listen(3000, () => {
+  console.log('server is listening');
+});
