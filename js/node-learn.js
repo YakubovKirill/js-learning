2fa3cee564191e6bd4ead516eeba155928242b92
Kirill <yakubov.kirill.work@gmail.com>
2020-09-15 22:19:31 +0300

    feat: basic modules

--- js/node-learn.js
+++ js/node-learn.js
@@ -1,2 +1,17 @@
 const chalk = require('chalk');
-console.log(chalk.red('Learn node'));
+const path = require('path');
+const os = require('os');
+const events = require('events');
+const { EventEmitter } = require('events');
+
+//console.log(path.parse(__filename));
+//console.log(os.platform());
+//console.log(os.cpus());
+//console.log(os.networkInterfaces());
+
+const emiter = new EventEmitter();
+emiter.on('any', data => {
+  console.log('On: ', data)
+});
+
+emiter.emit('any', {a: 1});
